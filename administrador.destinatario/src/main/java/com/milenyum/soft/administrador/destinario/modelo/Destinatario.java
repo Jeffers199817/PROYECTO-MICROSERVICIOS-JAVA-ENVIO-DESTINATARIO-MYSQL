35ae0c72ece7bd3fc66acb593fe71bd28c834bca
Jeffers199817 <jefferson.alquinga1998@gmail.com>
2024-12-24 23:18:47 -0500

    Integración de las listar de lista de envios

--- administrador.destinatario/src/main/java/com/milenyum/soft/administrador/destinario/modelo/Destinatario.java
+++ administrador.destinatario/src/main/java/com/milenyum/soft/administrador/destinario/modelo/Destinatario.java
@@ -1,11 +1,10 @@
 package com.milenyum.soft.administrador.destinario.modelo;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
+
+import java.util.List;
 
 @Setter
 @Getter
@@ -23,5 +22,7 @@ public class Destinatario {
     private String apellido;
     private String direccion;
     private String telefono;
+    @ElementCollection
+    private List<String> listaEnvios;
 
 }
